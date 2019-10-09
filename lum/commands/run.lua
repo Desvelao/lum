f82974af8e9e008ea5c9e8517e0f120ec79890a0
Desvelao <iamdesvelao@gmail.com>
2019-10-09 02:31:58 +0200

    run commands show scripts avaliables

--- lum/commands/run.lua
+++ lum/commands/run.lua
@@ -1,14 +1,21 @@
 return {
-    command = "run <command>",
+    command = "run [command]",
     description = "Run scripts commands like npm run. Require a lum_run.lua file in current directory",
     action = function(parsed, command, lum)
         local current_dir = lum.lfs.currentdir()
         package.path = current_dir.."/?.lua;"..package.path
         lum.pcall(function()
             local scripts = require("lum_run")
-            lum:execute(scripts[parsed.command], print)
+            if(parsed.command and scripts[parsed.command]) then
+                lum:execute(scripts[parsed.command], print)
+            else
+                lum.theme.primary("Scripts:")
+                for k,v in pairs(scripts) do
+                    lum.theme.secondary("  " ..k .. ": ".. v)
+                end
+            end
         end):fail(function(err)
-            lum.colorizer.pred(err)
+            lum.theme.error(err)
         end)
     end
 }

58f0617fa4f4ffbc1e6d0805023cd506ce210caa
Desvelao <iamdesvelao@gmail.com>
2019-10-10 22:10:08 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ error_exit "cp -r lum/lum $DEST_SRC" "Cannot copy lum src to $DEST_SRC"
 error_exit "cp lum/uninstall.sh $DEST_SRC/uninstall.sh" "Couldn't uninstall.sh to $DEST_SRC"
 chmod 777 $DEST_SRC/uninstall.sh
 echo "> Copied lum folder to $DEST_SRC"
-error_exit $CMD_INSTALL_DEPS "Could't install dependencies. Is it installed Luarocks?"
+error_exit "$CMD_INSTALL_DEPS" "Could't install dependencies. Is it installed Luarocks?"
 echo "> Installed dependencies"
 echo "lua5.3 -e 'lum_path=\"$DEST_SRC\";lum_env=\"$LUM_ENV\";package.path=\"$DEST_SRC/?.lua;$DEST_SRC/?/init.lua;$HOME/.luarocks/share/lua/5.1/?.lua;$HOME/.luarocks/share/lua/5.1/?/init.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;$HOME/.luarocks/share/lua/5.3/?.lua;$HOME/.luarocks/share/lua/5.3/?/init.lua;/usr/local/share/lua/5.3/?.lua;/usr/local/share/lua/5.3/?/init.lua;\"..package.path;package.cpath=\"$HOME/.luarocks/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/?.so;$HOME/.luarocks/lib/lua/5.3/?.so;/usr/local/lib/lua/5.3/?.so;\"..package.cpath' $DEST_SRC/lum.lua \"\$@\"" > $EXECUTABLE
 echo "> Created a executable on $EXECUTABLE"

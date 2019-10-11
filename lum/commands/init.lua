0e07023bbdf484853dc0eb7d0926413183eafe19
Desvelao <iamdesvelao@gmail.com>
2019-10-11 05:15:58 +0200

    install.sh detect lua version and lua interpeter

--- lum/commands/init.lua
+++ lum/commands/init.lua
@@ -4,13 +4,13 @@ return {
     action = function(parsed, command, lum)
         lum.pcall(function()
             local f = io.open("lum_run.lua", "w")
-        if(not f)then return end
-        f:write([[return {
-            -- add more scripts to run with `lum run <script_name>`
-            test = "ldoc ."
-        }]])
-        f:close()
-        lum.theme.success("Saved lum_run.lua!")
+            if(not f)then return end
+            f:write([[return {
+                -- add more scripts to run with `lum run <script_name>`
+                test = "ldoc ."
+            }]])
+            f:close()
+            lum.theme.success("Saved lum_run.lua!")
         end):fail(function(err)
             lum.theme.error("Ocurred an error")
         end)

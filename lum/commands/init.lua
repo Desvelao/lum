6e2a948c4ce8927772a7db41d078b6ff5d759646
Desvelao <iamdesvelao@gmail.com>
2019-10-26 05:04:38 +0200

    inclued how define scripts inner lum init script generated

--- lum/commands/init.lua
+++ lum/commands/init.lua
@@ -9,8 +9,19 @@ return {
             f:write(
 [[return {
     test = "echo \"Run this command with lum run test\"",
+    -- like_function = function(app, args)
+    --     app: cli instance
+    --     args: rest of arguments after script name. It's a table
+    -- end,
+    -- like_table = {
+        --     description = "Script table description",
+        --     run = "echo \"Hello\"" -- string/function. If it is a string, description is same like this
+        --     -- run = function(app, args)
+        --         -- ...
+        --     end
+        -- }
     -- add more scripts to run with `lum run <script_name>`
-    -- script can be a function with cli/app as first parameter. fn(app)
+    -- script can be a function with cli/app as first parameter. fn(app, args)
 }]])
             f:close()
             lum.theme.success("Saved lum_run.lua!")

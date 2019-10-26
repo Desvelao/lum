58b09223a346da7685f00069963761462f9edb2f
Desvelao <iamdesvelao@gmail.com>
2019-10-26 03:26:18 +0200

    fixed when there arent scripts

--- lum/commands/run.lua
+++ lum/commands/run.lua
@@ -11,30 +11,30 @@ return {
         local current_dir = lum.lfs.currentdir()
         local user_lum_dir = lum.methods.lum_home()
         local original_path = package.path
-        local scripts = {}
-        local function add_to_scripts(mode)
-            return function(scp)
-                for k,v in pairs(scp) do
-                    if(scripts[k] == nil) then scripts[k] = {mode = mode or "", command = v} end
+        local somescript = false
+        lum.pcall(function()   
+            local scripts = {}
+            local function add_to_scripts(mode)
+                return function(scp)
+                    for k,v in pairs(scp) do
+                        if(scripts[k] == nil) then scripts[k] = {mode = mode or "", command = v}; somescript = true end
+                    end
                 end
             end
-        end
-        lum.pcall(function()   
-
             lum.pcall(function() -- try to load local lum_run.lua
-                package.path = current_dir.."/?.lua;" .. original_path
+                package.path = current_dir.."/?.lua;"
                 return require("lum_run")
             end):pass(add_to_scripts("[./]")):done()
 
             lum.pcall(function() -- try to load home/<username>/.lum lum_run.lua
                 package.loaded["lum_run"] = nil -- unload from require chache the previous lu_run.lua file
-                package.path = user_lum_dir .. "/?.lua;" .. original_path
+                package.path = user_lum_dir .. "/?.lua;"
                 return require("lum_run")
             end):pass(add_to_scripts("[/.lum]")):done()
-            local ftable = require("f.table")
-            if(ftable.length(scripts) == 0) then error() end
+            if(not somescript) then error() end
             return scripts
         end):pass(function (scripts)
+            package.path = original_path
             if(parsed.command and scripts[parsed.command]) then
                 local script = scripts[parsed.command].command
                 if(type(script) == "string") then
